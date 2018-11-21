9d09db9161906fe348a9ab1f23d762eb04519f89
Marco Andrade <zmoky_260@hotmail.com>
2018-11-20 23:35:10 -0600

    Fin de la seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,16 +9,38 @@ export class InfoPaginaService {
   info: any = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor(private http: HttpClient) {
 
     // console.log('Servicio Pagina Lista');
+    // Leer archivo JSON
+    // this.http.get('assets/data/data-pagina.json')
+    // .subscribe(resp => {
+     //  this.cargada = true;
+      // this.info = resp;
+      // console.log(resp['email']);
+      // console.log(resp);
+    // });
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     // Leer archivo JSON
     this.http.get('assets/data/data-pagina.json')
     .subscribe(resp => {
       this.cargada = true;
       this.info = resp;
-      // console.log(resp['email']);
-      console.log(resp);
     });
+  }
+
+  private cargarEquipo() {
+     // Leer archivo JSON
+     this.http.get('https://angular-html-9325b.firebaseio.com/equipo.json')
+     .subscribe((resp: any) => {
+       this.equipo = resp;
+       // console.log(resp);
+     });
   }
 }
